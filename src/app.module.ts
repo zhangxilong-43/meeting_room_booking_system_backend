5a62d9715bce690b790b68b5ddaa3c7668734bb4
Xilonng Zhang <15575055655@163.com>
2024-09-10 22:24:38 +0800

    feat: 初始化项目

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+import { Permission } from "./user/entities/permission.entity";
+import { Role } from "./user/entities/role.entity";
+import { User } from "./user/entities/user.entity";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3307,
+      username: "root",
+      password: "123456",
+      database: "meeting_room_booking_system",
+      synchronize: true,
+      logging: true,
+      entities: [
+        Permission,
+        Role,
+        User,
+      ],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      }
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
