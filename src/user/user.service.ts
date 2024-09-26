5602e62fd0b4a81ff19a70439d1dda5a3a6f86d3
Xilonng Zhang <15575055655@163.com>
2024-09-26 22:09:00 +0800

    feat: 使用interceptor 用来对响应格式做转换 & 实现了修改信息、修改密码的接口

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,6 +17,8 @@ import { Role } from './entities/role.entity';
 import { Permission } from './entities/permission.entity';
 import { LoginUserVo } from './vo/login-user.vo';
 import { ConfigService } from '@nestjs/config';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 @Injectable()
 export class UserService {
@@ -143,6 +145,72 @@ export class UserService {
       }, []),
     };
   }
+
+  async findUserDetailById(userId: number) {
+    const user =  await this.userRepository.findOne({
+      where: {
+        id: userId
+      }
+    });
+    return user;
+  }
+
+  async updatePassword(userId: number, passwordDto: UpdateUserPasswordDto) {
+    const captcha = await this.redisService.get(`update_password_captcha_${passwordDto.email}`);
+    if(!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if(passwordDto.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.userRepository.findOneBy({
+      id: userId
+    });
+
+    foundUser.password = md5(passwordDto.password);
+
+    try {
+      await this.userRepository.save(foundUser);
+      return '密码修改成功';
+    } catch(e) {
+      this.logger.error(e, UserService);
+      return '密码修改失败';
+    }
+  }
+
+  async update(userId: number, updateUserDto: UpdateUserDto) {
+    const captcha = await this.redisService.get(`update_user_captcha_${updateUserDto.email}`);
+
+    if(!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if(updateUserDto.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.userRepository.findOneBy({
+      id: userId
+    });
+
+    if(updateUserDto.nickName) {
+      foundUser.nickName = updateUserDto.nickName;
+    }
+    if(updateUserDto.headPic) {
+      foundUser.headPic = updateUserDto.headPic;
+    }
+
+    try {
+      await this.userRepository.save(foundUser);
+      return '用户信息修改成功';
+    } catch(e) {
+      this.logger.error(e, UserService);
+      return '用户信息修改成功';
+    }
+  }
+
 
   async initData() {
     const user1 = new User();
